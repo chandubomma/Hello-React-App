3cd576eb8354e1bd69e28654cfb4744618bf931a
chandubomma <bommachandugoud@gmail.com>
2022-08-18 21:45:22 +0530

    my first hello react app which has a login page and says hello to the user when user login.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import image from "./images/galaxy2.jpg";
+import "./App.css";
+import { useState } from 'react';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  style={{backgroundImage: `url(${image})`,
+    height:'100vh',
+    backgroundSize:'cover',
+    backgroundRepeat : 'no-repeat',
+    backgroundPosition:'centre',
+    position: 'static'
+    }}>
+      <Form/>
     </div>
   );
+}
+
+function SayHello(props) {
+  return (
+    <div  style={{backgroundImage: `url(${image})`,
+    height:'100vh',
+    backgroundSize:'cover',
+    backgroundRepeat : 'no-repeat',
+    backgroundPosition:'centre',
+    position: 'static'
+    }}>
+      <p className='He'>Hello! {props.username}</p>
+    </div>
+  );
+}
+
+function submited(name){
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<SayHello username={name}/>);
+}
+
+function Form(){
+
+const [username,setUsername]=useState("");
+const [password,setPassword]=useState("");
+const handleSubmit = (event)=>{
+  event.preventDefault();
+  submited(username);
+  setPassword("");
+  setUsername("");
+}
+
+ return( <div style={{
+    backgroundColor:'darkgrey',
+    width:'25%',
+    height:'40%',
+    position:'absolute',
+    marginLeft:'35%',
+    marginTop:'15%',
+    
+  }}>
+    <p className='Login'>Login</p>
+    <form onSubmit={handleSubmit }>
+        <label className='Lab'>username:<input type="text" className='In'
+        name='username'
+        value={username}
+        onChange={(e)=>setUsername(e.target.value)}/>
+        </label>
+        <label className='Lab'>password: <input type="text" className='In'
+        name='password'
+        value={password}
+        onChange={(e)=> setPassword(e.target.value)}/>
+        </label>
+        <input type='submit' className='Submit'/>
+    </form>
+  </div>);
 }
 
 export default App;
